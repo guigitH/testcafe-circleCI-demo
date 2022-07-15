350556a30a1b53f616556126f32e91cfc43ff775
Guillaume <slack77@hotmail.fr>
2022-07-15 18:32:33 +0200

    Create testing.md file

--- tests/example/example.ts
+++ tests/example/example.ts
@@ -3,8 +3,20 @@ import { Selector } from 'testcafe';
 fixture `Examples of tests`
     .page `http://devexpress.github.io/testcafe/example`;
 
-test('Enter name and click populate', async t => {
-    await t.typeText('input[id=developer-name', 'Peter Clark')
+test.meta({id: "1657929369"})('User can populate a name', async t => {
     await t.setNativeDialogHandler(() => true)
     await t.click('input[id=populate]')
+});
+
+
+test.meta({id: "1657929369"})('', async t => {
+});
+
+test.meta({id: "1657929369"})('', async t => {
+});
+
+test.meta({id: "1657929369"})('', async t => {
+});
+
+test.meta({id: "1657929369"})('', async t => {
 });
