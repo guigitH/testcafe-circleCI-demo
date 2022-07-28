3a84e2deaed1e8e5f1eb8547f3fc38283268d841
Guillaume <slack77@hotmail.fr>
2022-07-28 20:39:32 +0200

    Fix eslint issues

--- tests/example/example.ts
+++ tests/example/example.ts
@@ -1,33 +1,31 @@
-import { Selector } from 'testcafe';
 import ExamplePage from '../pages/ExamplePage';
 import ThankYouPage from '../pages/ThankYouPage';
 import { URL } from '../constants/Url';
 
 fixture `Examples of tests`
-    .page `${URL.example}`;
+	.page `${URL.example}`;
 
-test.meta({id: "1657929369"})('User can populate a name and submit the form', async t => {
-    const examplePage = new ExamplePage(t)
-    const thankYouPage = new ThankYouPage(t)
+test.meta({id: '1657929369'})('User can populate a name and submit the form', async t => {
+	const examplePage = new ExamplePage(t);
+	const thankYouPage = new ThankYouPage(t);
 
-    await examplePage.setNativeHandler()
-    await examplePage.clickPopulate()
-    await examplePage.clickSubmit()
-    await thankYouPage.confirmHeaderText('Thank you, Peter Parker!')
+	await examplePage.setNativeHandler();
+	await examplePage.clickPopulate();
+	await examplePage.clickSubmit();
+	await thankYouPage.confirmHeaderText('Thank you, Peter Parker!');
 });
 
 
-test.meta({id: "1657929369"})('User cannot submit the form without valid data', async t => {
-    const examplePage = new ExamplePage(t)
+test.meta({id: '1657929369'})('User cannot submit the form without valid data', async t => {
+	const examplePage = new ExamplePage(t);
 
-    await examplePage.clickSubmit()
-    await examplePage.confirmUrl(URL.example)
+	await examplePage.clickSubmit();
+	await examplePage.confirmUrl(URL.example);
 });
 
-test.meta({id: "1657929369"})
-    .page(`${URL.thankYou}`)
-    .requestHooks(ThankYouPage.errorCssResponse)
-    ('Html page can be loaded with css error response', async t => {
-    const thankYouPage = new ThankYouPage(t)
-    await thankYouPage.confirmHeaderText('Thank you')
-});
+test.meta({id: '1657929369'})
+	.page(`${URL.thankYou}`)
+	.requestHooks(ThankYouPage.errorCssResponse)('Html page can be loaded with css error response', async t => {
+		const thankYouPage = new ThankYouPage(t);
+		await thankYouPage.confirmHeaderText('Thank you');
+	});
--- tests/pages/ThankYouPage.ts
+++ tests/pages/ThankYouPage.ts
@@ -1,19 +1,19 @@
-import BasePage from "./BasePage";
-import { Selector } from "testcafe";
-import { mockResponseWithError } from "../helpers/MockApi";
+import BasePage from './BasePage';
+import { Selector } from 'testcafe';
+import { mockResponseWithError } from '../helpers/MockApi';
 
 class ThankYouPage extends BasePage {
-    private header: Selector = Selector('h1[id="article-header"]');
+	private header: Selector = Selector('h1[id="article-header"]');
     
-    public static errorCssResponse = mockResponseWithError(
-        /https:\/\/devexpress.github.io\/testcafe\/css\/example-page.css/,
-        'GET',
-        500,
-      );
+	public static errorCssResponse = mockResponseWithError(
+		/https:\/\/devexpress.github.io\/testcafe\/css\/example-page.css/,
+		'GET',
+		500,
+	);
       
-    public async confirmHeaderText(text: string): Promise<TestControllerPromise> {
-        await this.controller.expect(this.header.innerText).contains(text)
-    }
+	public async confirmHeaderText(text: string): Promise<TestControllerPromise> {
+		await this.controller.expect(this.header.innerText).contains(text);
+	}
 }
 
-export default ThankYouPage
+export default ThankYouPage;
